65a0269e0d10db303ad4586c27a17ee6a34a375c
Bradley Bossard <bradleybossard@gmail.com>
2014-10-19 14:24:30 -0700

    Adding Hello World to app using BitmapFont.  Got Desktop version of app working in Android Studio

--- core/src/com/bradleybossard/libgdxgame/MainActivity.java
+++ core/src/com/bradleybossard/libgdxgame/MainActivity.java
@@ -2,26 +2,52 @@ package com.bradleybossard.libgdxgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainActivity extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+    BitmapFont font;
+	//Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        font = new BitmapFont();
+        font.setColor(Color.RED);
+        //font.setScale(30.0f);
+		//img = new Texture("badlogic.jpg");
 	}
 
-	@Override
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+
+    @Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
+        font.draw(batch, "Hello World", 200, 200);
 		batch.end();
 	}
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
 }
--- desktop/src/com/bradleybossard/libgdxgame/desktop/DesktopLauncher.java
+++ desktop/src/com/bradleybossard/libgdxgame/desktop/DesktopLauncher.java
@@ -4,9 +4,30 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.bradleybossard.libgdxgame.MainActivity;
 
+//import sun.jvm.hotspot3.HelloWorld;
+
 public class DesktopLauncher {
+    /*
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MainActivity(), config);
 	}
+    */
+
+
+    //public class Main {
+        public static void main(String[] args) {
+            LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+            cfg.title = "hello-world";
+            //cfg.useGL30 = true;
+            //cfg.useGL20 = false;
+            cfg.width = 480;
+            cfg.height = 320;
+
+            //new LwjglApplication(new HelloWorld(), cfg);
+            new LwjglApplication(new MainActivity(), cfg);
+
+        }
+    //}
+
 }
